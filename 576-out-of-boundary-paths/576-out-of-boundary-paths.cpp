8cb5fd16b9eeaee68f1fd90e67205006ac356b80
Aditi375 <60137420+Aditi375@users.noreply.github.com>
2022-07-16 17:49:04 +0530

    Time: 8 ms (84.71%), Space: 6.6 MB (71.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
+    
     int dp[55][55][55];
-    long long mod = 1e9 + 7;
-    int dfs(int i, int j, int n, int m, int x){
-        
-        if(i < 0 || i >= n || j < 0 || j >= m)
+    int dfs(int x,int m , int n , int i,int j)
+    {
+        long long mod=pow(10,9)+7;
+        if(i<0||i>=m||j<0||j>=n)
+        {
             return 1;
-        if(x <= 0)
+        }
+        if(x<=0)
+        {
             return 0;
-        if(dp[i][j][x] != -1)
+        }
+        if(dp[i][j][x]!=-1)
+        {
             return dp[i][j][x];
-        
-        int up = dfs(i-1,j,n,m,x-1);
-        int down = dfs(i+1,j,n,m,x-1);
-        int left = dfs(i,j-1,n,m,x-1);
-        int right = dfs(i,j+1,n,m,x-1);
-        int total = (up % mod + down % mod + left % mod + right % mod) % mod;
-        dp[i][j][x] = total;
+        }
+        int up=dfs(x-1,m,n,i-1,j);
+        int down=dfs(x-1,m,n,i+1,j);
+        int left=dfs(x-1,m,n,i,j-1);
+        int right=dfs(x-1,m,n,i,j+1);
+        int total=(up%mod+down%mod+left%mod+right%mod)%mod;
+        dp[i][j][x]=total;
         return dp[i][j][x];
     }
-    
-    int findPaths(int n, int m, int maxMove, int Row, int Column) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(Row,Column,n,m,maxMove);
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+       
+       memset(dp,-1,sizeof(dp));
+      
+        
+      int res=dfs(maxMove,m , n ,startRow,startColumn);
+        return res;
+        
     }
 };
