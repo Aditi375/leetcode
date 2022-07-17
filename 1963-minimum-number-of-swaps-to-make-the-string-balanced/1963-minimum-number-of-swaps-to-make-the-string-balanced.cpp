f229e42ed1e9888d8507855b2b1707537890c42d
Aditi375 <60137420+Aditi375@users.noreply.github.com>
2022-07-17 16:08:20 +0530

    Time: 275 ms (8.05%), Space: 41.5 MB (6.13%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int minSwaps(string s) {
+        
+        /*
+        
+        approach 1
         int m=0;
         int curr=0;
-        
-        
         for(int i=0;i<s.length();i++)
         {
             if(s[i]==']')
@@ -20,5 +22,32 @@ public:
              m=max(curr,m);
         }
         return (m+1)/2;
+        */
+        
+        
+        //approach 2 using stack
+        //if we make a single swapit will affect 2 closing brackets
+        
+        stack<int> st;
+        
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if(!st.empty())
+                {
+                    st.pop();
+                }
+            }
+        }
+        
+        return (st.size()+1)/2;
+        
+        
     }
 };
